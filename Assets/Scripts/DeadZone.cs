4f87bcbd23053a03a94678ebea53872a18f15e54
PhoenixGameDev69 <PhoenixGameDev69@gmail.com>
2022-05-03 15:38:17 +0300

    Materials added

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -3,8 +3,17 @@ using UnityEngine.Events;
 
 public class DeadZone : MonoBehaviour
 {
+    [SerializeField] private float _lavaSpeed;
     [SerializeField] private GameObject _deadParticle;
     [SerializeField] private UnityEvent _onPlayerEnterTrigger;
+    [SerializeField] private Material _lavaMaterial;
+
+    private void Update()
+    {
+        _lavaMaterial.mainTextureOffset = new Vector2(_lavaMaterial.mainTextureOffset.x, _lavaMaterial.mainTextureOffset.y - Time.deltaTime * _lavaSpeed);
+
+        if (_lavaMaterial.mainTextureOffset.y <= -10) _lavaMaterial.mainTextureOffset = Vector2.zero;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
